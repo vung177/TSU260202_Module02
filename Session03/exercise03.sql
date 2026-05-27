e943c10951b26c30f01a03b26bbca79cff2b8170
vung177 <vu.ng177@gmail.com>
2026-05-27 21:34:31 +0900

    updated exercise03 sql

--- Session03/exercise03.sql
+++ Session03/exercise03.sql
@@ -1,28 +1,25 @@
 USE module2_sql;
-DROP TABLE IF EXISTS MuonSach;
-DROP TABLE IF EXISTS Sach;
-DROP TABLE IF EXISTS DocGia;
-
-CREATE TABLE DocGia (
+DROP TABLE IF EXISTS borrow;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS readers;
+CREATE TABLE readers (
     reader_id INT AUTO_INCREMENT PRIMARY KEY,
     reader_name VARCHAR(100) NOT NULL,
     phone VARCHAR(15)
 );
-
-CREATE TABLE Sach (
+CREATE TABLE books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     book_title VARCHAR(200) NOT NULL,
     author VARCHAR(100)
 );
-
-CREATE TABLE MuonSach (
+CREATE TABLE borrow (
     borrow_id INT AUTO_INCREMENT PRIMARY KEY,
     reader_id INT NOT NULL,
     book_id INT NOT NULL,
     borrow_date DATE,
     return_date DATE,
-    FOREIGN KEY (reader_id) REFERENCES DocGia(reader_id),
-    FOREIGN KEY (book_id) REFERENCES Sach(book_id)
+    FOREIGN KEY (reader_id) REFERENCES readers(reader_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
-
-ALTER TABLE MuonSach MODIFY borrow_date DATE NOT NULL;
+ALTER TABLE borrow
+MODIFY borrow_date DATE NOT NULL;
