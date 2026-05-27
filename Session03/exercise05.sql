ce1d1856b37203373b13739ccb58485fb6f88b34
vung177 <vu.ng177@gmail.com>
2026-05-27 22:52:39 +0900

    updated

--- Session03/exercise05.sql
+++ Session03/exercise05.sql
@@ -1,20 +1,18 @@
+USE module2_sql;
 DROP TABLE IF EXISTS enrollments;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS teachers;
 DROP TABLE IF EXISTS students;
-
 CREATE TABLE teachers (
     teacher_id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
-
 CREATE TABLE students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
-
 CREATE TABLE courses (
     course_id INT AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(150) NOT NULL,
@@ -23,7 +21,6 @@ CREATE TABLE courses (
     teacher_id INT NOT NULL,
     FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
 );
-
 CREATE TABLE enrollments (
     student_id INT NOT NULL,
     course_id INT NOT NULL,
@@ -31,5 +28,5 @@ CREATE TABLE enrollments (
     FOREIGN KEY (student_id) REFERENCES students(student_id),
     FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
-
-ALTER TABLE courses ADD CONSTRAINT chk_course_price CHECK (price > 0);
+ALTER TABLE courses
+ADD CONSTRAINT chk_course_price CHECK (price > 0);
