7087cfee94f251a4ff4c6a2ba9833c9d983f00cf
vung177 <vu.ng177@gmail.com>
2026-05-27 22:27:16 +0900

    updated

--- Session03/exercise04.sql
+++ Session03/exercise04.sql
@@ -1,5 +1,5 @@
+USE module2_sql;
 DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -7,5 +7,5 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     status VARCHAR(20) DEFAULT 'active'
 );
-
-ALTER TABLE users ADD CONSTRAINT chk_status CHECK (status IN ('active', 'inactive', 'banned'));
+ALTER TABLE users
+ADD CONSTRAINT chk_status CHECK (status IN ('active', 'inactive', 'banned'));
