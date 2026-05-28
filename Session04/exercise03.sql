05c8d23120d4bd63bab50c61f42255db61c12cc9
vung177 <vu.ng177@gmail.com>
2026-05-28 11:46:45 +0900

    updated

--- Session04/exercise03.sql
+++ Session04/exercise03.sql
@@ -1,17 +1,21 @@
 -- 1. Hiển thị sinh viên có năm sinh từ 2003 đến 2005
 -- Chỉ hiển thị: mã sinh viên (student_id), họ tên (student_name), ngày sinh (dob)
-SELECT student_id, student_name, dob 
-FROM students 
+SELECT student_id,
+    student_name,
+    dob
+FROM students
 WHERE YEAR(dob) BETWEEN 2003 AND 2005;
-
--- 2. Hiển thị sinh viên có giới tính là Nam hoặc Nữ (trong database lưu dưới dạng 'Male' hoặc 'Female')
+-- 2. Hiển thị sinh viên có giới tính là Nam hoặc Nữ
 -- Chỉ hiển thị: mã sinh viên (student_id), họ tên (student_name), ngày sinh (dob)
-SELECT student_id, student_name, dob 
-FROM students 
+SELECT student_id,
+    student_name,
+    dob
+FROM students
 WHERE gender IN ('Male', 'Female');
-
--- 3. Hiển thị sinh viên có mã sinh viên thuộc một trong các mã: SV001, SV004, SV005 (tương ứng student_id là 1, 4, 5)
+-- 3. Hiển thị sinh viên có mã sinh viên là các mã sau: SV001, SV004, SV005
 -- Chỉ hiển thị: mã sinh viên (student_id), họ tên (student_name), ngày sinh (dob)
-SELECT student_id, student_name, dob 
-FROM students 
+SELECT student_id,
+    student_name,
+    dob
+FROM students
 WHERE student_id IN (1, 4, 5);
